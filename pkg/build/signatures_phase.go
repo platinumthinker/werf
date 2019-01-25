99fd2f208e2e0696ae486a078f75ba49e7df5e7a
Timofey Kirillov <timofey.kirillov@flant.com>
2019-01-25 16:17:55 +0300

    go

--- pkg/build/signatures_phase.go
+++ pkg/build/signatures_phase.go
@@ -23,95 +23,86 @@ func NewSignaturesPhase() *SignaturesPhase {
 type SignaturesPhase struct{}
 
 func (p *SignaturesPhase) Run(c *Conveyor) (err error) {
-	logger.LogServiceProcess("Calculate signatures", "", func() error {
-		err = p.run(c)
-		return err
-	})
+	for _, image := range c.imagesInOrder {
+		err = logger.LogServiceProcess(fmt.Sprintf("Calculate %s signatures", image.GetName()), " ", func() error {
+			return p.calculateImageSignatures(c, image)
+		})
 
-	return
-}
-
-func (p *SignaturesPhase) run(c *Conveyor) error {
-	if debug() {
-		fmt.Printf("SignaturesPhase.Run\n")
+		if err != nil {
+			return err
+		}
 	}
 
-	for _, image := range c.imagesInOrder {
-		if debug() {
-			fmt.Printf("  image: '%s'\n", image.GetName())
+	return nil
+}
+
+func (p *SignaturesPhase) calculateImageSignatures(c *Conveyor, image *Image) error {
+	var prevStage stage.Interface
+
+	image.SetupBaseImage(c)
+
+	var prevBuiltImage imagePkg.ImageInterface
+	prevImage := image.GetBaseImage()
+	err := prevImage.SyncDockerState()
+	if err != nil {
+		return err
+	}
+
+	var newStagesList []stage.Interface
+
+	for _, s := range image.GetStages() {
+		if prevImage.IsExists() {
+			prevBuiltImage = prevImage
 		}
 
-		var prevStage stage.Interface
+		isEmpty, err := s.IsEmpty(c, prevBuiltImage)
+		if err != nil {
+			return fmt.Errorf("error checking stage %s is empty: %s", s.Name(), err)
+		}
+		if isEmpty {
+			logger.LogInfoF("%20s: -\n", s.Name())
+			continue
+		}
 
-		image.SetupBaseImage(c)
-
-		var prevBuiltImage imagePkg.ImageInterface
-		prevImage := image.GetBaseImage()
-		err := prevImage.SyncDockerState()
+		stageDependencies, err := s.GetDependencies(c, prevImage)
 		if err != nil {
 			return err
 		}
 
-		var newStagesList []stage.Interface
+		checksumArgs := []string{stageDependencies, BuildCacheVersion}
 
-		// FIXME: block dimg name
-
-		for _, s := range image.GetStages() {
-			if prevImage.IsExists() {
-				prevBuiltImage = prevImage
-			}
-
-			isEmpty, err := s.IsEmpty(c, prevBuiltImage)
-			if err != nil {
-				return fmt.Errorf("error checking stage %s is empty: %s", s.Name(), err)
-			}
-			if isEmpty {
-				logger.LogInfoF("Ignore empty stage %s\n", s.Name())
-				continue
-			}
-
-			if debug() {
-				fmt.Printf("    %s\n", s.Name())
-			}
-
-			stageDependencies, err := s.GetDependencies(c, prevImage)
-			if err != nil {
-				return err
-			}
-
-			checksumArgs := []string{stageDependencies, BuildCacheVersion}
-
-			if prevStage != nil {
-				checksumArgs = append(checksumArgs, prevStage.GetSignature())
-			}
-
-			stageSig := util.Sha256Hash(checksumArgs...)
-
-			s.SetSignature(stageSig)
-
-			imageName := fmt.Sprintf(LocalImageStageImageFormat, c.projectName(), stageSig)
-			logger.LogInfoF("%s/%s image name %s\n", image.GetName(), s.Name(), imageName)
-			i := c.GetOrCreateImage(prevImage, imageName)
-			s.SetImage(i)
-
-			if err = i.SyncDockerState(); err != nil {
-				return fmt.Errorf("error synchronizing docker state of stage %s: %s", s.Name(), err)
-			}
-
-			if err = s.AfterImageSyncDockerStateHook(c); err != nil {
-				return err
-			}
-
-			newStagesList = append(newStagesList, s)
-
-			prevStage = s
-			prevImage = i
+		if prevStage != nil {
+			checksumArgs = append(checksumArgs, prevStage.GetSignature())
 		}
 
-		image.SetStages(newStagesList)
+		stageSig := util.Sha256Hash(checksumArgs...)
 
-		fmt.Println() // FIXME
+		s.SetSignature(stageSig)
+
+		imageName := fmt.Sprintf(LocalImageStageImageFormat, c.projectName(), stageSig)
+
+		logger.LogInfoF("%20s: %s\n", s.Name(), imageName)
+
+		i := c.GetOrCreateImage(prevImage, imageName)
+		s.SetImage(i)
+
+		if err = i.SyncDockerState(); err != nil {
+			return fmt.Errorf("error synchronizing docker state of stage %s: %s", s.Name(), err)
+		}
+
+		if err = s.AfterImageSyncDockerStateHook(c); err != nil {
+			return err
+		}
+
+		newStagesList = append(newStagesList, s)
+
+		prevStage = s
+		prevImage = i
 	}
+
+	image.SetStages(newStagesList)
+
+	fmt.Println() // FIXME
 
 	return nil
 }
